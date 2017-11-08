748c44b3c03e216b59bf026fc022a92e65186988
trushing <33245972+trushing@users.noreply.github.com>
2017-11-08 07:45:08 -0600

    final
    final

--- controls.cpp
+++ controls.cpp
@@ -50,11 +50,11 @@ void computeMatricesFromInputs(){
 	glfwGetCursorPos(window, &xpos, &ypos);
 
 	// Reset mouse position for next frame
-	glfwSetCursorPos(window, 1024/2, 768/2);
+	glfwSetCursorPos(window, 800/2, 600/2);
 
 	// Compute new orientation
-	horizontalAngle += mouseSpeed * float(1024/2 - xpos );
-	verticalAngle   += mouseSpeed * float( 768/2 - ypos );
+	horizontalAngle += mouseSpeed * float(800/2 - xpos );
+	verticalAngle   += mouseSpeed * float( 600/2 - ypos );
 
 	// Direction : Spherical coordinates to Cartesian coordinates conversion
 	glm::vec3 direction(
